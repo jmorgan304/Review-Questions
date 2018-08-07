e736b7fa668d279da20a033b9f3fe3d200a7e0bb
Josh Morgan <jmorgan304@gmail.com>
2018-08-07 13:09:19 -0500

    Updated Question constructor, behavior of insertValues, and overrode toString. Added polynomial creation to CalculusQuestion.

--- src/CalculusQuestion.java
+++ src/CalculusQuestion.java
@@ -1,7 +1,65 @@
+import java.util.ArrayList;
+import java.util.List;
 
-public class CalculusQuestion extends Question{
-	
-	CalculusQuestion(String template) {
-		super(template);
+public class CalculusQuestion extends Question {
+
+	public static void main(String args[]) {
+		String template = "Find the derivative of <0>";
+		String[] values = { randomIntegerPolynomial(5) };
+		CalculusQuestion cq = new CalculusQuestion(template, values);
+		System.out.println(cq);
+	}
+
+	CalculusQuestion(String template, String[] values) {
+		super(template, values);
+	}
+
+	public static String randomIntegerPolynomial(int highestPower) {
+		// Moves from highest power to 0 with 50% chance of picking each integer power
+		// between
+		// Also picks a constant in (0, 9] for each term
+		List<Integer> constants = new ArrayList<>();
+		List<Integer> powers = new ArrayList<>();
+
+		for (int i = highestPower; i >= 0; i--) {
+			if (eventOccurs(.5)) {
+				constants.add(randomInteger(1, 10));
+				powers.add(i);
+			}
+		}
+
+		return buildPolynomial(constants, powers);
+	}
+
+	public static <T extends Number> String buildPolynomial(List<T> constants, List<T> powers)
+			throws IllegalArgumentException {
+		if (constants.size() != powers.size()) {
+			throw new IllegalArgumentException("Number of constants must equal the number of powers.");
+		}
+		String polynomial = "";
+		for (int i = 0; i < constants.size(); i++) {
+			polynomial += constants.get(i);
+
+			if (powers.get(i).doubleValue() == 0d) {
+				continue;
+			}
+			else if (powers.get(i).doubleValue() == 1d) {
+				polynomial += "x";
+			}
+			else {
+				polynomial += "x^" + powers.get(i);
+			}
+
+			if (i < constants.size() - 1) {
+				if (eventOccurs(.5)) {
+					polynomial += " - ";
+				}
+				else {
+					polynomial += " + ";
+				}
+			}
+		}
+
+		return polynomial;
 	}
 }
--- src/Question.java
+++ src/Question.java
@@ -7,44 +7,47 @@ public class Question {
 	protected String template;
 	protected String[] parsedTemplate;
 	protected int numValues;
-	
-	
+	protected String completedQuestion;
+
 	public static void main(String[] args) {
 		String template = "<0> Test <1> text <2> for the question <3> <> <<>> <12345> <12d34> <5>";
-		Question q = new Question(template);
-		System.out.println(q.insertValues(new String[]{"0", "1", "2", "3", "4", "5"}));
+		Question q = new Question(template, new String[] { "0", "1", "2", "3", "4", "5" });
+		System.out.println(q);
+		q.insertValues(new String[] { "5", "6", "7", "8", "9", "10" });
+		System.out.println(q);
 	}
-	
-	public Question(String template) {
+
+	public Question(String template, String[] values) {
 		this.template = template;
 		this.parsedTemplate = parseTemplate(template);
 		this.numValues = countMatches(template);
+		insertValues(values);
 	}
-	
+
 	public static String[] parseTemplate(String template) {
 		// Every substring with the form < + any number of digits + >
 		return pattern.split(template);
 	}
-	
+
 	public static int countMatches(String template) {
 		Matcher matcher = pattern.matcher(template);
-		
+
 		int numMatches = 0;
-		while(matcher.find()) {
+		while (matcher.find()) {
 			numMatches++;
 		}
 		return numMatches;
 	}
-	
+
 	public static int randomInteger(int lower, int upper) throws IllegalArgumentException {
 		int range = upper - lower;
 		if (range < 0) {
 			throw new IllegalArgumentException("Upper value must be greater than lower");
 		}
 		double randomValue = Math.random();
-		return (int)(range * randomValue + lower);
+		return (int) (range * randomValue + lower);
 	}
-	
+
 	public static double randomReal(double lower, double upper) throws IllegalArgumentException {
 		double range = upper - lower;
 		if (range < 0d) {
@@ -53,7 +56,7 @@ public class Question {
 		double randomValue = Math.random();
 		return range * randomValue + lower;
 	}
-	
+
 	public static boolean eventOccurs(double probabilityOfSuccess) throws IllegalArgumentException {
 		if (probabilityOfSuccess > 1d || probabilityOfSuccess < 0d) {
 			throw new IllegalArgumentException("Probabilities must be in [0, 1]");
@@ -66,18 +69,21 @@ public class Question {
 			return false;
 		}
 	}
-	
-	public String insertValues(String[] values) throws IllegalArgumentException {
+
+	public void insertValues(String[] values) throws IllegalArgumentException {
 		if (values.length != this.numValues) {
 			throw new IllegalArgumentException("Incorrect number of values");
 		}
-		String completeQuestion = "";
+		String completedQuestion = "";
 		for (int i = 0; i < this.numValues; i++) {
-			completeQuestion += parsedTemplate[i];
-			completeQuestion += values[i];
+			completedQuestion += parsedTemplate[i];
+			completedQuestion += values[i];
 		}
-		return completeQuestion;
+		this.completedQuestion = completedQuestion;
 	}
-	
-	
+
+	public String toString() {
+		return this.completedQuestion;
+	}
+
 }
