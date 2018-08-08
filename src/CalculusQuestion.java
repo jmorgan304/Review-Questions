edcb4db1a5b52fe97c85e8d8667c9b0fc4df395e
Josh Morgan <jmorgan304@gmail.com>
2018-08-08 16:53:55 -0500

    Added support for multi-variable integer polynomials.

--- src/CalculusQuestion.java
+++ src/CalculusQuestion.java
@@ -2,10 +2,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CalculusQuestion extends Question {
+	private static final char[] variables = { 'x', 'y', 'z', 'u', 'v', 't', 'w' };
 
 	public static void main(String args[]) {
 		String template = "Find the derivative of <0>";
-		String[] values = { randomIntegerPolynomial(5) };
+		String[] values = { randomIntegerPolynomial(10, 3, 2) };
 		CalculusQuestion cq = new CalculusQuestion(template, values);
 		System.out.println(cq);
 	}
@@ -14,40 +15,46 @@ public class CalculusQuestion extends Question {
 		super(template, values);
 	}
 
-	public static String randomIntegerPolynomial(int highestPower) {
+	public static String randomIntegerPolynomial(int highestPower, int numOfTerms, int numOfVariables) {
 		// Moves from highest power to 0 with 50% chance of picking each integer power
 		// between
 		// Also picks a constant in (0, 9] for each term
-		List<Integer> constants = new ArrayList<>();
-		List<Integer> powers = new ArrayList<>();
+		List<Integer> powers = generateIntegerPowers(highestPower, numOfTerms, numOfVariables);
+		List<Integer> constants = generateIntegerConstants(numOfTerms);
 
-		for (int i = highestPower; i >= 0; i--) {
-			if (eventOccurs(.5)) {
-				constants.add(randomInteger(1, 10));
-				powers.add(i);
-			}
-		}
-
-		return buildPolynomial(constants, powers);
+		return buildPolynomial(constants, powers, numOfVariables);
 	}
 
-	public static <T extends Number> String buildPolynomial(List<T> constants, List<T> powers)
+	public static <T extends Number> String buildPolynomial(List<T> constants, List<T> powers, int numOfVariables)
 			throws IllegalArgumentException {
-		if (constants.size() != powers.size()) {
-			throw new IllegalArgumentException("Number of constants must equal the number of powers.");
+		// Powers are indexed according to vars per term, so if there are 2 vars per
+		// term, the first two powers go with the first term
+		if (constants.size() != (powers.size() / numOfVariables)) {
+			throw new IllegalArgumentException(
+					"Number of constants must equal (number of powers / number of variables).");
+		}
+		if (numOfVariables > variables.length) {
+			throw new IllegalArgumentException("Not enough unique variables available.");
 		}
 		String polynomial = "";
-		for (int i = 0; i < constants.size(); i++) {
-			polynomial += constants.get(i);
 
-			if (powers.get(i).doubleValue() == 0d) {
-				continue;
+		for (int i = 0; i < constants.size(); i++) {
+			if (Double.valueOf(constants.get(i).toString()) != 1d) {
+				polynomial += constants.get(i);
 			}
-			else if (powers.get(i).doubleValue() == 1d) {
-				polynomial += "x";
-			}
-			else {
-				polynomial += "x^" + powers.get(i);
+			for (int j = 0; j < numOfVariables; j++) {
+				char varLetter = variables[j];
+				int powerIndex = i * j + i;
+
+				if (powers.get(powerIndex).doubleValue() == 0d) {
+					polynomial += "";
+				}
+				else if (powers.get(powerIndex).doubleValue() == 1d) {
+					polynomial += varLetter;
+				}
+				else {
+					polynomial += varLetter + "^" + powers.get(powerIndex);
+				}
 			}
 
 			if (i < constants.size() - 1) {
@@ -61,5 +68,35 @@ public class CalculusQuestion extends Question {
 		}
 
 		return polynomial;
+	}
+
+	private static List<Integer> generateIntegerPowers(int highestPower, int numOfTerms, int numOfVariables) {
+		List<Integer> powers = new ArrayList<>();
+
+		int totalPowers = numOfTerms * numOfVariables;
+		int termCount = 0;
+		int currentPower = highestPower;
+
+		while (termCount < totalPowers) {
+			if (eventOccurs(.5)) {
+				powers.add(currentPower);
+				termCount++;
+				currentPower--;
+			}
+			double prob = (totalPowers / ((double) totalPowers + termCount));
+			if (eventOccurs(prob)) {
+				currentPower--;
+			}
+		}
+
+		return powers;
+	}
+
+	private static List<Integer> generateIntegerConstants(int numberOfConstants) {
+		List<Integer> constants = new ArrayList<>();
+		for (int i = 0; i < numberOfConstants; i++) {
+			constants.add(randomInteger(1, 10));
+		}
+		return constants;
 	}
 }
